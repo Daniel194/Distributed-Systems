22aed08fd017ad1444e4ec9df3839e248b367430
Daniel194 <lungu.daniel94@gmail.com>
2017-01-04 18:02:10 +0200

    Berkeley Algorithm - Refactoring.

--- problems/berkeley.go
+++ problems/berkeley.go
@@ -6,7 +6,6 @@ import (
 	"net"
 	"encoding/binary"
 	"time"
-	"strings"
 	"math/rand"
 )
 
@@ -16,18 +15,13 @@ type slaveNode struct {
 	delta     int64
 }
 
-type slaveResponse struct {
-	addr      *net.UDPAddr
-	timeTicks int64
-}
-
 func main() {
 
 	slaves := []string{"localhost:3333", "localhost:4444", "localhost:5555"}
 
-	go runSlave(slaves[0])
-	go runSlave(slaves[1])
-	go runSlave(slaves[2])
+	go runSlave(slaves[0], "Slave #0")
+	go runSlave(slaves[1], "Slave #1")
+	go runSlave(slaves[2], "Slave #2")
 	go runMaster("localhost:2222", slaves)
 
 	var input string
@@ -44,9 +38,6 @@ func checkError(isFatal bool, err error) {
 }
 
 func runMaster(address string, slavesList []string) {
-	fmt.Println("Beginning Clock Synchronization...")
-	fmt.Println("Creating UDP socket to request slave nodes times")
-
 	serverAddr, err := net.ResolveUDPAddr("udp", address)
 	checkError(true, err)
 
@@ -56,105 +47,70 @@ func runMaster(address string, slavesList []string) {
 
 	slaveNodes := make([]*slaveNode, len(slavesList))
 
-	for {
-		for i, slaveAddr := range slavesList {
-			tempNodeAddr, err := net.ResolveUDPAddr("udp", slaveAddr)
-			if err != nil {
-				fmt.Println(err)
-				continue
-			}
-			slaveNodes[i] = &slaveNode{addr: tempNodeAddr}
+	for i, slaveAddr := range slavesList {
+		tempNodeAddr, err := net.ResolveUDPAddr("udp", slaveAddr)
+
+		if err != nil {
+			fmt.Println(err)
+			continue
 		}
 
-		// Poll all slave nodes for their time
-		masterDelta := time.Now().Unix()
-		fmt.Printf("Before adjustment: %v\n", time.Unix(masterDelta, 0))
-		slaveResponses := pollSlaves(slaveNodes, sock)
-
-		// Compute the algorithm for nodes that have responded
-		masterDelta = berkeleyTime(masterDelta, slaveResponses, slaveNodes)
-		fmt.Printf("After adjustment: %v\n", time.Unix(masterDelta, 0))
-
-		// Send the nodes their new time deltas
-		tellTheSlaves(slaveNodes, sock)
-		// Sleep
-		time.Sleep(5 * time.Second)
+		slaveNodes[i] = &slaveNode{addr: tempNodeAddr}
 	}
+
+	// Poll all slave nodes for their time
+	masterDelta := time.Now().Unix()
+	fmt.Printf("Master before adjustment: %v\n", time.Unix(masterDelta, 0))
+
+	pollSlaves(slaveNodes, sock)
+
+	fmt.Print("\n ================================= \n \n")
+
+	// Compute the algorithm for nodes that have responded
+	masterDelta = calculateTime(masterDelta, slaveNodes)
+	fmt.Printf("Master after adjustment: %v\n", time.Unix(masterDelta, 0))
+
+	// Send the nodes their new time deltas
+	tellTheSlaves(slaveNodes, sock)
+
 }
 
-func pollSlaves(slaves []*slaveNode, masterSock *net.UDPConn) chan slaveResponse {
-	res := make(chan slaveResponse, len(slaves))
+func pollSlaves(slaves []*slaveNode, masterSock *net.UDPConn) {
+
 	for _, slave := range slaves {
 		masterSock.WriteToUDP([]byte("Master requesting slave time"), slave.addr)
-		go func(address *net.UDPAddr) {
-			buf := make([]byte, 1024)
-			fmt.Println("Making request to ", address)
-			n, addr, err := masterSock.ReadFromUDP(buf)
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
 
-			// Err is number of bytes read, == 0: buf too small, < 0: overflow
-			ticks, bytes := binary.Varint(buf[:n])
-			if bytes <= 0 {
-				return
-			}
-			fmt.Printf("Received %v from %v\n", time.Unix(ticks, 0), addr)
-			res <- slaveResponse{addr: address, timeTicks: ticks}
-		}(slave.addr)
+		buf := make([]byte, 1024)
+		n, _, err := masterSock.ReadFromUDP(buf)
+
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+
+		ticks, bytes := binary.Varint(buf[:n])
+
+		if bytes <= 0 {
+			return
+		}
+
+		slave.timeTicks = ticks
+
 	}
 
-	return res
 }
 
-func berkeleyTime(now int64, slaveResponses chan slaveResponse, slaves []*slaveNode) int64 {
+func calculateTime(now int64, slaves []*slaveNode) int64 {
 	totalTime := now
-
-	timeouts := make(chan bool, len(slaves))
-	for _ = range slaves {
-		go func() {
-			time.Sleep(1 * time.Second)
-			timeouts <- true
-		}()
-	}
-
-	var responses []slaveResponse
-	for _ = range slaves {
-		select {
-		case <-timeouts:
-			break
-		case res := <-slaveResponses:
-			responses = append(responses, res)
-			break
-		}
-	}
-
 	numResponses := 1
-	fmt.Printf("%v - Master\n", time.Unix(totalTime, 0))
-	for i, nodeRes := range responses {
-		if nodeRes.timeTicks != 0 {
-			numResponses++
-
-			fmt.Printf("%v - Slave #%v\n", time.Unix(nodeRes.timeTicks, 0), i)
-
-			totalTime += nodeRes.timeTicks
-			for _, node := range slaves {
-				if node.addr.String() == nodeRes.addr.String() {
-					node.timeTicks = nodeRes.timeTicks
-				}
-			}
-		}
-	}
-
-	// 1 response is only master
-	fmt.Printf("%v node(s) responded.\n", numResponses - 1)
 	var masterDelta int64
-	if numResponses > 1 {
-		masterDelta = totalTime / int64(numResponses)
-	} else {
-		masterDelta = now
+
+	for _, slave := range slaves {
+		numResponses++
+		totalTime += slave.timeTicks
 	}
+
+	masterDelta = totalTime / int64(numResponses)
 
 	for _, node := range slaves {
 		node.delta = masterDelta - node.timeTicks
@@ -164,23 +120,21 @@ func berkeleyTime(now int64, slaveResponses chan slaveResponse, slaves []*slaveN
 }
 
 func tellTheSlaves(slaves []*slaveNode, sock *net.UDPConn) {
+
 	for _, node := range slaves {
 		writeBuf := make([]byte, 1024)
 		bytes := binary.PutVarint(writeBuf, node.delta)
+
 		if bytes <= 0 {
 			fmt.Println("Error encoding the node's delta")
 			continue
 		}
 
-		fmt.Println("Making request to ", node.addr)
 		sock.WriteToUDP(writeBuf, node.addr)
 	}
 }
 
-func runSlave(address string) {
-	fmt.Println("Beginning Clock Synchronization...")
-	fmt.Printf("Creating UDP socket for %s\n", address)
-
+func runSlave(address string, name string) {
 	serverAddr, err := net.ResolveUDPAddr("udp", address)
 	checkError(true, err)
 
@@ -190,48 +144,45 @@ func runSlave(address string) {
 
 	var delta int64
 
-	// This rng added is for testing locally
 	rand.Seed(time.Now().UTC().UnixNano())
 
-	for {
-		fmt.Printf("Listening at %v\n", serverAddr)
-		readBuf := make([]byte, 1024)
-		n, addr, err := sock.ReadFromUDP(readBuf)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
+	readBuf := make([]byte, 1024)
+	n, addr, err := sock.ReadFromUDP(readBuf)
 
-		randomTime := time.Duration(rand.Intn(600)) * time.Second
-		fmt.Printf("%v random seconds added\n", randomTime)
-		now := time.Now().Add(randomTime).Unix()
-		fmt.Printf("Before adjustment: %v\n", time.Unix(now + delta, 0))
-
-		msg := strings.TrimSpace(string(readBuf[:n]))
-		fmt.Println(msg)
-
-		writeBuf := make([]byte, 1024)
-		bytes := binary.PutVarint(writeBuf, now)
-		if bytes <= 0 {
-			fmt.Println("Error encoding time")
-			continue
-		}
-
-		fmt.Println("Making request to ", addr)
-		sock.WriteToUDP(writeBuf, addr)
-
-		readBuf = make([]byte, 1024)
-		n, addr, err = sock.ReadFromUDP(readBuf)
-		if err != nil {
-			fmt.Println(err)
-			continue
-		}
-
-		delta, bytes = binary.Varint(readBuf[:n])
-		if bytes <= 0 {
-			continue
-		}
-		fmt.Printf("Received adjustment of %v\n", delta)
-		fmt.Printf("After adjustment %v\n", time.Unix(now + delta, 0))
+	if err != nil {
+		fmt.Println(err)
+		return
 	}
+
+	randomTime := time.Duration(rand.Intn(600)) * time.Second
+	now := time.Now().Add(randomTime).Unix()
+	fmt.Printf("%v before adjustment: %v \n", name, time.Unix(now + delta, 0))
+
+	writeBuf := make([]byte, 1024)
+	bytes := binary.PutVarint(writeBuf, now)
+
+	if bytes <= 0 {
+		fmt.Println("Error encoding time")
+		return
+	}
+
+	sock.WriteToUDP(writeBuf, addr)
+
+	readBuf = make([]byte, 1024)
+	n, addr, err = sock.ReadFromUDP(readBuf)
+
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	delta, bytes = binary.Varint(readBuf[:n])
+
+	if bytes <= 0 {
+		return
+	}
+
+	fmt.Printf("%v received adjustment of %v \n", name, delta)
+	fmt.Printf("%v after adjustment %v\n", name, time.Unix(now + delta, 0))
+
 }
